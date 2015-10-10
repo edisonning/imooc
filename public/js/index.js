a555a8f5fce63c1872b66a635ca70e4bb04909c4
ningbo <18898600@qq.com>
2015-10-10 17:16:26 +0800

    前端样式

---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+RevSlide.initRevolutionSlider();
+      $(window).load(function() {
+          $('[data-zlname = reverse-effect]').mateHover({
+              position: 'y-reverse',
+              overlayStyle: 'rolling',
+              overlayBg: '#fff',
+              overlayOpacity: 0.7,
+              overlayEasing: 'easeOutCirc',
+              rollingPosition: 'top',
+              popupEasing: 'easeOutBack',
+              popup2Easing: 'easeOutBack'
+          });
+      });
+      $(window).load(function() {
+          $('.flexslider').flexslider({
+              animation: "slide",
+              start: function(slider) {
+                  $('body').removeClass('loading');
+              }
+          });
+      });
+      //    fancybox
+      jQuery(".fancybox").fancybox();
